94308d2e472fae13ab324d78cecede834671b8cf
mrange <marten_range@hotmail.com>
2018-09-19 23:10:41 +0200

    Quiet

--- i4k_OGLShader/src/mzk.cpp
+++ i4k_OGLShader/src/mzk.cpp
@@ -15,7 +15,9 @@ void mzk_init( short *buffer )
         const float fl = sinf( 6.2831f*440.0f * (float)i/(float)MZK_RATE );
         const float fr = sinf( 6.2831f*587.3f * (float)i/(float)MZK_RATE );
 
-        buffer[2*i+0] = f2i(fl*32767.0f);
-        buffer[2*i+1] = f2i(fr*32767.0f);
+        //buffer[2*i+0] = f2i(fl*32767.0f);
+        //buffer[2*i+1] = f2i(fr*32767.0f);
+        buffer[2*i+0] = 0.0;
+        buffer[2*i+1] = 0.0;
     }
 }
